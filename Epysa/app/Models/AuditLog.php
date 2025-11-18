b5f83726e8fc1888905bfcbbbbb1ad417d281454
CChavez720 <107702096+CChavez720@users.noreply.github.com>
2025-11-18 16:06:23 -0300

    Ajuste minimo en AuditLog
    Se arregla barra de busqueda.

--- Epysa/app/Models/AuditLog.php
+++ Epysa/app/Models/AuditLog.php
@@ -23,4 +23,12 @@ class AuditLog extends Model
         'valores_antes' => 'array',
         'valores_despues' => 'array'
     ];
+
+    // =========================================================
+    // RELACIONES
+    // =========================================================
+    public function usuario()
+    {
+        return $this->belongsTo(User::class, 'usuario_id', 'id_us');
+    }
 }
